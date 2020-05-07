72b1eff350800f83b4777cb1017fdc20f0dbf6e4
Chaisir Ibnu Supraja <pkusumanegara@gmail.com>
2020-05-07 21:52:04 +0700

    OTP

--- resources/views/app/register/home/index.blade.php
+++ resources/views/app/register/home/index.blade.php
@@ -59,12 +59,36 @@
             <div class="col-md-6">
             </div>
             <div class="col-md-6">
-                <button aria-label="" class="btn btn-cons m-t-10" type="submit">Login</button>
-                <button aria-label="" class="btn btn-primary btn-cons m-t-10" type="submit">Buat Akun Baru</button>
+                <a href="/login" class="btn btn-cons m-t-10" >Login</a>
+                <a href="#modalConfirm" class="btn btn-primary m-t-10" type="submit" data-toggle="modal" data-record-id="1" data-record-name="">Buat Akun Baru</a>
             </div>
           </div>
           <br>
         </form>
       </div>
+    </div>
+    <div class="modal fade slide-up disable-scroll" id="modalConfirm" tabindex="-1" role="dialog" aria-hidden="false">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content-wrapper">
+                <div class="modal-content">
+                    <div class="modal-header clearfix text-left">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true"><i class="fas fa-times fs-14"></i>
+                        </button>
+                        <h5>Konfirmasi <span class="semi-bold">User</span></h5>
+                        <p class="p-b-10">Masukkan kode otentikasi dikirim ke nomor telepon atau email</p>
+                        <input type="text" class="form-control" />
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-md-8">
+                            </div>
+                            <div class="col-md-4 m-t-10 sm-m-t-10">
+                                <a href="/" class="btn btn-block btn-primary btn-cons m-b-10"><i class="fas fa-check"></i> &nbsp;&nbsp; Confirm</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
