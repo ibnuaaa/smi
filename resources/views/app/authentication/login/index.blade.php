b6b8614eb052ca9a16006d05973b0cdaa939f9ad
Waldi Irawan <waldiirawan127@gmail.com>
2020-05-07 12:40:09 +0700

    aall

--- resources/views/app/authentication/login/index.blade.php
+++ resources/views/app/authentication/login/index.blade.php
@@ -8,6 +8,7 @@
         <div class="login-container bg-white" style="width: 496px;display: block;position: relative;margin: 0px auto;float:unset;padding-top: 5%;">
             <div class="p-l-50 m-l-20 p-r-50 m-r-20 p-t-0 m-t-0 sm-p-l-15 sm-p-r-15 sm-p-t-0 text-center">
                 <img src="/assets/img/logo.png">
+                <h1>REFINA</h1>
                 <form id="loginForm" class="p-t-15" role="form">
                     <div class="form-group form-group-default">
                         <div class="controls">
@@ -21,12 +22,18 @@
                     </div>
                     <div class="row">
                         <div class="col-md-12 text-center m-t-10">
+                            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                            <label for="vehicle1"> Ingat Saya</label><br>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12 text-center m-t-10">
                             <button class="btn btn-primary m-t-10 w-100" type="submit">Sign in</button>
                         </div>
                     </div>
                     <div class="row">
                         <div class="col-md-12 text-center m-t-10">
-                            <a href="/register" class="text-info small">Register</a>
+                            <a href="/register" class="text-info">Registrasi Baru</a>
                         </div>
                     </div>
                 </form>
